d63de2d790d3f9aa3a7000e273efa381f8f550c8
jayson <jaysonj@163.com>
2020-03-05 21:24:04 +0800

    fix(location): can't start listen bug

--- thingsboard_gateway/gateway/tb_twisted_service.py
+++ thingsboard_gateway/gateway/tb_twisted_service.py
@@ -1,6 +1,7 @@
+import threading
 from threading import Thread
 
-from twisted.internet import reactor, endpoints
+from twisted.internet import reactor, endpoints, defer
 from logging import getLogger
 
 from thingsboard_gateway.protocols.usr.usr_protocol_factory import UsrProtocolFactory
@@ -14,24 +15,26 @@ log = getLogger("service")
 
 
 class TBTwistedService(object):
-    _instance = None
+    _instance_lock = threading.Lock()
 
     def __new__(cls, *args, **kw):
-        if cls._instance is None:
-            cls._instance = object.__new__(cls, *args, **kw)
-            # 异步线程启动twisted服务
-            from twisted.internet import reactor
-            Thread(target=reactor.run, args=(False,)).start()
-            log.debug('Start TwistedService')
-            cls.__ports = {}
-            cls.__frames = {
-                'rtu': ModbusRtuFramer,
-                'ascii': ModbusAsciiFramer,
-                'socket': ModbusSocketFramer,
-                'binary': ModbusBinaryFramer,
-                'tls': ModbusTlsFramer
-            }
-        return cls._instance
+        if not hasattr(TBTwistedService, "_instance"):
+            with TBTwistedService._instance_lock:
+                if not hasattr(TBTwistedService, "_instance"):
+                    TBTwistedService._instance = object.__new__(cls)
+                    log.info('Start TwistedService')
+                    cls.__ports = {}
+                    cls.__frames = {
+                        'rtu': ModbusRtuFramer,
+                        'ascii': ModbusAsciiFramer,
+                        'socket': ModbusSocketFramer,
+                        'binary': ModbusBinaryFramer,
+                        'tls': ModbusTlsFramer
+                    }
+                    # 异步线程启动twisted服务
+                    from twisted.internet import reactor
+                    Thread(target=reactor.run, args=(False,)).start()
+        return TBTwistedService._instance
 
     def __init__(self):
         pass
@@ -44,81 +47,92 @@ class TBTwistedService(object):
         description = 'tcp:%d' % port
 
         # 先获取是否有listen
-        client = self.__ports[description]
-        listen = client['listen']
-        if listen is not None:
-            factory = client['factory']
-            number = client['number']
-            number += 1
-            factory.updateDevices(config.get('devices'))
-            self.__ports[description] = {'listen': listen, 'factory': factory, 'number': number}
-            log.debug("%s connector is start listened %s", (config['name'], description))
-            log.debug("%d connector is listened %s" % (number, description))
-            return
+        client = self.__ports.get(description, None)
+        if client is not None:
+            listen = client.get('listen', None)
+            if listen is not None:
+                factory = client['factory']
+                number = client['number']
+                number += 1
+                factory.updateDevices(config.get('devices'), connector)
+                self.__ports[description] = {'listen': listen, 'factory': factory, 'number': number}
+                log.info("%s connector is start listened %s", (config['name'], description))
+                log.info("%d connector is listened %s" % (number, description))
+                return
 
         # 新增listen
         # TODO: 暂时支持Usr
         if protocol == 'usr':
-            factory = UsrProtocolFactory(gateway, connector, self.__frames[config['method']])
-            factory.addDevices(config.get('devices'))
+            factory = UsrProtocolFactory(gateway, connector, None, framer=self.__frames[config['method']])
+            factory.addDevices(config, connector)
 
         if factory is None:
             raise Exception("Can't add listen, protocol %s is not support", protocol)
 
-        def _add_listen(des, fact):
-            log.debug("%s connector is start listened %s", (config['name'], description))
-            endpoint = endpoints.serverFromString(reactor, des)
-            new_listen = endpoint.listen(fact)
-            self.__ports[description] = {'listen': new_listen, 'factory': fact, 'number': 1}
-
         # 不同线程需要使用此twisted的API来操作reactor
-        reactor.callFromThread(_add_listen, description, factory)
+        reactor.callFromThread(self._add_listen, description, factory)
+
+    def _add_listen(self, description, factory):
+        log.info("Start listened %s", description)
+        endpoint = endpoints.serverFromString(reactor, description)
+        new_listen = endpoint.listen(factory)
+        self.__ports[description] = {'listen': new_listen, 'factory': factory, 'number': 1}
 
     def rem_listen(self, config):
         port = config['port']
         description = 'tcp:%d' % port
 
-        def _rem_listen(des):
-            client = self.__ports[des]
-            listen = client['listen']
+        # 不同线程需要使用此twisted的API来操作reactor
+        reactor.callFromThread(self._rem_listen, config, description)
+
+    def _rem_listen(self, config, description):
+        client = self.__ports.get(description, None)
+        if client is not None:
+            listen = client.get('listen', None)
             if listen is not None:
                 f = client['factory']
                 number = client['number']
                 number -= 1
                 f.removeDevices(config)
                 if number <= 0:
-                    log.debug("Stop listen %s" % description)
+                    log.info("Stop listen %s" % description)
                     del self.__ports[description]
                     if listen is not None:
                         listen.result.stopListening()
                 else:
-                    log.debug("%s connector is stop listened %s", (config['name'], description))
-                    log.debug("Still %d connector is listened %s" % (number, description))
-
-        # 不同线程需要使用此twisted的API来操作reactor
-        reactor.callFromThread(_rem_listen, description)
+                    log.info("%s connector is stop listened %s", (config['name'], description))
+                    log.info("Still %d connector is listened %s" % (number, description))
 
     def clear_listen(self):
-        log.debug("Clear listen")
-        for _, v in self.__ports.items():
+        for k, v in self.__ports.items():
+            log.info("Stop listen %s" % k)
+
             def _clear_listen(listen):
                 # stop listen
                 if listen is not None:
                     listen.result.stopListening()
 
             # 不同线程需要使用此twisted的API来操作reactor
-            reactor.callFromThread(_clear_listen, v)
+            reactor.callFromThread(_clear_listen, v['listen'])
         self.__ports.clear()
+        log.info("Clear listen")
+        reactor.callFromThread(reactor.stop)
 
     def rpc_handler(self, config, content):
         port = config['port']
         description = 'tcp:%d' % port
-        _, factory, _ = self.__ports[description]
-        if factory is not None:
-            # Usr 协议
-            if isinstance(factory, UsrProtocolFactory):
-                factory.server_side_rpc_handler(config, content)
+        client = self.__ports.get(description, None)
+        if client is not None:
+            factory = client.get('factory')
+            if factory is not None:
+                # Usr 协议
+                if isinstance(factory, UsrProtocolFactory):
+                    factory.server_side_rpc_handler(config, content)
+            else:
+                log.error("Received rpc request, but factory not found in config for %s.",
+                          description,
+                          config["name"])
         else:
-            log.error("Received rpc request, but factory not found in config for %s.",
+            log.error("Received rpc request, but client not found in config for %s.",
                       description,
                       config["name"])
